db5cd8f6bd4c426d4ac53856f55178b7c0752345
VaibhavMathur03 <mathurvaibhav010@gmail.com>
2024-02-23 01:45:59 +0530

    delete page route

--- page/page.services.js
+++ page/page.services.js
@@ -11,7 +11,19 @@ export const listPages = async () => {
   const pages = await Pages.find({});
   return pages;
 };
-export const deletePage = async (pageId) => {};
+export const deletePage = async (pageId) => {
+  try {
+    const deletedPage = await Pages.findByIdAndDelete(pageId);
+    
+    if (!deletedPage) {
+      throw new Error('Page not found');
+    }
+    return deletedPage;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const updatePage = async (pageId, pageBody) => {};
 export const pageDetails = async (pageId) => {
   const pages = await Pages.findOne({ _id: pageId });
